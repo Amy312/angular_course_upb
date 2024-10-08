bc52ca59ff48ca677fc30f77f72ed642a58bef11
Amy312 <katsukiamy1@gmail.com>
2024-10-08 18:55:36 -0400

    [08-10-24] Completed architecture

---
+++ src/app/client/client2/client2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'client2',
+  templateUrl: './client2.component.html',
+  styleUrl: './client2.component.css'
+})
+export class Client2Component {
+
+}
---
+++ src/app/shared/shared2-directiva/shared2-directiva.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared2-directiva',
+  templateUrl: './shared2-directiva.component.html',
+  styleUrl: './shared2-directiva.component.css'
+})
+export class Shared2DirectivaComponent {
+
+}
